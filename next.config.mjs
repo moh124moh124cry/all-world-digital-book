47c49c929674af81a2f237dd662031dbf93f9ada
moh124moh124cry <cherfaouimohamed377@gmail.com>
2026-08-08 00:28:12 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { unoptimized: true },
   webpack: (config) => {
     config.resolve.alias.canvas = false
     return config
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Frame-Options', value: 'ALLOWALL' },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://web.telegram.org https://*.telegram.org",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
+    
