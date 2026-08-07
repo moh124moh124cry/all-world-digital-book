24ff40eafe96f79c3bfecf26fd34d5a28d2e9f05
moh124moh124cry <cherfaouimohamed377@gmail.com>
2026-08-07 21:20:53 +0100

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: { unoptimized: true },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false
+    return config
+  },
+}
+
+export default nextConfig
